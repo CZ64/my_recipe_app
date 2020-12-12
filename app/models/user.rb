8895a9bcd41beda5f3bcd964e8b611626caafbd1
CZ64 <cz6delta@gmail.com>
2020-12-13 03:44:34 +0900

    #45 update user signup/login view

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,15 @@ class User < ApplicationRecord
 
   has_many :recipes,          dependent: :destroy
   has_many :user_ingredients, dependent: :destroy
-  has_many :menus,             dependent: :destroy
+  has_many :menus,            dependent: :destroy
   has_many :favorite_recipes, dependent: :destroy
   has_many :recipes_through_favorite, through: :favorite_recipes, source: :recipe
 
+  # validations
+  validates :name,  presence: true, uniqueness: true, length: { in: 3..8 }
+  validates :servings_for,  presence: true,
+                            numericality: {
+                              only_integer: true,
+                              greater_than_or_equal_to: 1
+                            }
 end
