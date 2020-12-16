6d0cb512cf62007dedb7176569f76fb1351eac50
CZ64 <cz6delta@gmail.com>
2020-12-16 23:25:37 +0900

    #6 test user registration/login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :recipes, dependent: :destroy
+
+  # validation
+  validates :name,  presence: true, length: { maximum: 8 }
 end
--- config/application.rb
+++ config/application.rb
@@ -18,10 +18,10 @@ module MyRecipeApp
 
     config.generators do |g|
       g.test_framework :rspec,
-              fixtures: false,
               view_specs: false,
               helper_specs: false,
               routing_specs: false
+              # fixtures: false,
       end
   end
 end
